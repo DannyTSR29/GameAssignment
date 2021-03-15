0495fb552381ad19be54319b6b1a3acba07b4a26
TangYikChin <66994561+TangYikChin@users.noreply.github.com>
2021-03-15 23:30:29 +0800

    push

--- GamePractical/Background.cpp
+++ GamePractical/Background.cpp
@@ -3,6 +3,7 @@
 Background::Background() {
 	sprite = NULL;
 	texture = NULL;
+	texture1 = NULL;
 }
 
 Background::~Background() {
@@ -21,6 +22,11 @@ void Background::Init() {
 		D3DX_DEFAULT, D3DX_DEFAULT, D3DCOLOR_XRGB(255, 255, 255),
 		NULL, NULL, &texture);
 
+	D3DXCreateTextureFromFileEx(GameGraphic::getInstance()->getDevice(), "basketballco.jpg", D3DX_DEFAULT, D3DX_DEFAULT,
+		D3DX_DEFAULT, NULL, D3DFMT_A8R8G8B8, D3DPOOL_MANAGED,
+		D3DX_DEFAULT, D3DX_DEFAULT, D3DCOLOR_XRGB(255, 255, 255),
+		NULL, NULL, &texture1);
+
 	//	Create font. Study the documentation.
 	D3DXCreateFont(GameGraphic::getInstance()->getDevice(), 25, 0, 0, 1, false,
 		DEFAULT_CHARSET, OUT_TT_ONLY_PRECIS, DEFAULT_QUALITY,
@@ -31,16 +37,30 @@ void Background::Init() {
 	spriteRect.right = 1252;
 	spriteRect.bottom = 624;
 
+	spriteRect1.left = 0;
+	spriteRect1.top = 0;
+	spriteRect1.right = 1252;
+	spriteRect1.bottom = 624;
+
 
 	textRect.left = 0;
 	textRect.top = -100;
 	textRect.right = 0;
 	textRect.bottom = 0;
 
+	positionA.x = 100;
+	positionA.y = 100;
+
 }
 
 void Background::Update() {
-
+	
+	if (isMoving)
+	{
+		animationTimer += 1 / 60.0f;
+		D3DXVECTOR2 velocity = direction * (speed / 60.0f);
+		position += velocity;
+	}
 	
 }
 
@@ -54,6 +74,10 @@ void Background::Draw() {
 	//BACKGROUND
 	//	Sprite rendering. Study the documentation.
 	sprite->Draw(texture, &spriteRect, NULL, NULL, D3DCOLOR_XRGB(255, 255, 255));
+	D3DXMatrixTransformation2D(&mat, NULL, 0.0, NULL, NULL, NULL, &positionA);
+	sprite->SetTransform(&mat);
+	sprite->Draw(texture1, &spriteRect1, NULL, NULL, D3DCOLOR_XRGB(255, 255, 255));
+	
 
 	/*string hpStr = to_string(GameWin::getInstance()->hp);
 	font->DrawText(sprite, hpStr.c_str(), -1, &textRect, DT_CENTER | DT_NOCLIP, D3DCOLOR_XRGB(255, 255, 255));*/
--- GamePractical/Background.h
+++ GamePractical/Background.h
@@ -5,8 +5,10 @@ class Background
 {
 private:
 	LPDIRECT3DTEXTURE9 texture;
+	LPDIRECT3DTEXTURE9 texture1;
 	LPD3DXSPRITE sprite;
 	RECT spriteRect;
+	RECT spriteRect1;
 	LPD3DXFONT font;
 	RECT textRect;
 
@@ -15,7 +17,14 @@ private:
 	D3DXVECTOR2 spriteCentre;
 	D3DXVECTOR2 trans;
 	D3DXVECTOR2 scaling;
+	D3DXVECTOR2 direction;
+	D3DXVECTOR2 position;
+	D3DXVECTOR2 directionmove;
+	D3DXVECTOR2 positionA;
 	float rotation;
+	bool isMoving;
+	float speed;
+	float animationTimer;
 
 public:
 	Background();
